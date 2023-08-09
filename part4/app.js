007e61b70d9cff18bbc73feb82212028e2da8b09
asraf-mridha <asrafmridha@gmail.com>
2023-08-09 23:05:43 +0600

    Data and attribute binding

---
+++ part4/app.js
@@ -0,0 +1,12 @@
+var app = Vue.createApp({
+  data() {
+    return {
+        tutorialInfo: {
+            name: "Vue Js 3",
+            githubLink: "https://github.com/asrafmridha",
+        },
+    }
+  },
+});
+
+app.mount("#app");
