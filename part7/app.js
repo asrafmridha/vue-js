69aaac7d11d110a4cccea59f576216c39f6c7962
asraf-mridha <asrafmridha@gmail.com>
2023-08-10 11:33:37 +0600

    Event Modifiers form handling div event

--- part7/app.js
+++ part7/app.js
@@ -8,6 +8,20 @@ var app = Vue.createApp({
     handlekeyup(ev) {
       this.name = ev.target.value;
     },
+    handleFormSubmit() {
+      // e.preventDefault();
+      console.log("form Submitted");
+    },
+    handleCardEvent(e) {
+      console.log(e);
+      console.log("Card Clicked");
+
+    },
+    handleCardViewEvent(e) {
+      e.stopPropagation(); 
+      console.log('View Clicked');
+      console.log(e);
+    },
   },
 });
 
