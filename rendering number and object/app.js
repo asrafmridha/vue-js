249c780304907d3d955c6bb0f7567b70947b321f
asraf-mridha <asrafmridha@gmail.com>
2023-08-12 10:20:46 +0600

    List rendering of object and numbers

---
+++ rendering number and object/app.js
@@ -0,0 +1,16 @@
+var app = Vue.createApp({
+  data() {
+    return {
+      aboutMe :{
+          name: "Tahmina Toma",
+          nickName: "ashfia",
+          website: "https://globaltechgiant.com",
+        },
+    };
+  },
+  methods: {
+
+  },
+});
+
+app.mount("#app");
